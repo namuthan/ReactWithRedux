1125aa29c0018683f6b77e66a91ff1e55d616602
namuthan <nabilbbb33@gmail.com>
2016-05-30 21:58:31 -0600

    updated initial App Structure

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class AboutPage extends React.Component {
+class CoursesPage extends React.Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1>About Page</h1>
-        <p> This is the About Page </p>
+        <h1>Courses Page</h1>
+        <p> This is the Courses Page </p>
       </div>
     );
   }
 }
 
 
-export default AboutPage;
+export default CoursesPage;
