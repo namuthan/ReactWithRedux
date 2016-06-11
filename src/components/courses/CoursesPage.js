cef705f46d5a5325fe1c867e9baf2b2543d907b8
namuthan <nabilbbb33@gmail.com>
2016-06-10 20:55:48 -0600

    Basic setup of react with redux application

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,16 +1,68 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
+
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: {title: ""}
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    //fire up an action
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
   render() {
+    //debugger;
     return (
       <div className="jumbotron">
         <h1>Courses Page</h1>
         <p> This is the Courses Page </p>
+        {this.props.courses.map(this.courseRow)}
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value = {this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
 }
 
+CoursesPage.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  courses: React.PropTypes.array.isRequired
+};
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+  //debugger;
+  return {
+    courses: state.courses
+  };
+}
+
+
+export default connect(mapStateToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 import 'babel-polyfill'; //transpile what babel can't
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import {Router, browserHistory } from 'react-router';
 // browserHistory - gives us nice clean urls
 
@@ -10,7 +12,11 @@ import routes from './routes';
 import './styles/styles.css'; //Webpack can import css files
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
